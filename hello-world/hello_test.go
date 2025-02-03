1bd0e23db699f67eef010c5c3463069112551cde
Przemysław Piotrowski <przemyslaw.piotrowski@atteo.com>
2025-02-03 18:48:35 +0100

    Hello world

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,8 +3,37 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris!"
+	t.Run("saying hello with a name supplied", func(t *testing.T) {
+		got := Hello("Chris", "")
+		want := "Hello, Chris!"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("saying hello without a name supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World!"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("saying hello with a name supplied in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie!"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("saying hello with a name supplied in French", func(t *testing.T) {
+		got := Hello("Louis", "French")
+		want := "Bonjour, Louis!"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
